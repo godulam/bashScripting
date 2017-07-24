6d1ff81cc01ae2904acd70ef32e8261f70f333c3
godulam <maciek.godula@gmail.com>
2017-07-24 07:42:46 +0000

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+var1=23
+echo "var1= $var1"
+
+# read parameter
+echo "Give parameter a"
+read a
+echo "a= $a"
+
+# dos
+echo "var2= $var2"
+# donts
+echo var2= $var2
+echo var2= ${var2}
+
+c="a b  c d"
+echo $c
+echo "$c"
